375d75084e1eb14dcc0ab26ae72a88421340f076
jaeyeah <limjh819@naver.com>
2025-12-06 22:52:46 +0900

    기본 홈 화면

--- final/finalproject/src/components/Content.jsx
+++ final/finalproject/src/components/Content.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Test from "./Contents/Test";
+import Home from "./Home";
 import MemberJoin from "./member/MemberJoin";
 import SearchAndSave from "./Contents/SearchAndSave";
 
@@ -18,6 +19,10 @@ export default function Content() {
 
                 {/* 분할된 화면의 주소를 지정하는 영역 (path=주소 , element=화면) */}
                 <Routes>
+                    {/* 메인 페이지 */}
+                    <Route path="/" element={<Home/>}></Route>
+                    
+
                     {/* contents */}
                     <Route path="/contents/test" element={<Test/>}></Route>
                     <Route path="/contents/test2" element={<SearchAndSave/>}></Route>
---
+++ final/finalproject/src/components/Home.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+export default function Home() {
+    return(<>
+        <div className="row mt-4">
+            <div className="col">
+                <h1>홈 화면</h1>
+                <p>환영합니다!</p>
+            </div>
+        </div>
+    </>)
+}
